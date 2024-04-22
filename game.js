0bafefe8b24c768a0b2ef35155a5b2a6f71e586f
Aidensupboiiiiii <91507962+Aidensupboiiiiii@users.noreply.github.com>
2024-04-22 16:12:24 -0500

    File upload

---
+++ game.js
@@ -0,0 +1,55 @@
+let score=0
+let playagain
+let devpassword
+let devpassnum
+let devtrue
+function DevClicked(){
+    if (devtrue){
+        devtrue=false
+    }else{
+        devtrue=true
+    }
+}
+function game(){
+let randomnum=(Math.floor(Math.random()*100)+1);
+console.log(randomnum)
+
+if(devtrue==true){
+   devpassword=prompt("enter your password given by aiden");
+   if (devpassword == "aidenc434"){
+    alert("the code is " + randomnum);
+   }else if(devpassword=="chey452"){
+    alert("the code is " + randomnum)
+   }
+}
+
+while(true){
+    let guess=prompt("guess the number");
+    if(guess === null){
+        alert("you canceled")
+        alert("the number was " + randomnum)
+        console.log("you scored " + score + " times")
+        break;
+    } else {
+        let int=parseInt(guess);
+        if(int == randomnum){
+            alert("You guessed correctly");
+            score+=1
+            alert("you have scored " + score + " times")
+           playagain=confirm("would you like to play again? Your score of " + score + " will reset if no.")
+           if(playagain==true){
+            game()
+           } else{
+            console.log("you scored "+score+" times")
+           }
+            break;
+        }
+        else if(int < randomnum){
+            alert("Higher, guess again");
+        }
+        else if(int > randomnum){
+            alert("Lower, guess again")
+        }
+    }
+}
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is my first game<br>
+i hope you enjoy and if you would like to become a dev for the game just dm me on discord<br>
+my discord is: direvr
